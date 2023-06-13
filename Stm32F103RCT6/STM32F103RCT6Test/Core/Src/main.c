c224670ea9a7d5e78c3d3e388b27642a3e6144db
Paul_Lin <lisie31s@gmail.com>
2023-06-13 16:07:14 +0800

    chore: The file generation process using CubeMX should not cause any conflicts with the existing compilation architecture.
    note:
    	1. Change linkscript file to original to custom file (custom_flash.ld)
    	2. Let CubeMX don't generate the main function.

--- Stm32F103RCT6/STM32F103RCT6Test/Core/Src/main.c
+++ Stm32F103RCT6/STM32F103RCT6Test/Core/Src/main.c
@@ -56,56 +56,17 @@ static void MX_NVIC_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
-
+int main(void){
+  while(1){
+    rt_thread_mdelay(100);
+  }
+}
 /* USER CODE END 0 */
 
 /**
   * @brief  The application entry point.
   * @retval int
   */
-int main(void)
-{
-  /* USER CODE BEGIN 1 */
-
-  /* USER CODE END 1 */
-
-  /* MCU Configuration--------------------------------------------------------*/
-
-  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
-  // HAL_Init();
-
-  /* USER CODE BEGIN Init */
-
-  /* USER CODE END Init */
-
-  /* Configure the system clock */
-  // SystemClock_Config();
-
-  /* USER CODE BEGIN SysInit */
-
-  /* USER CODE END SysInit */
-
-  /* Initialize all configured peripherals */
-  // MX_GPIO_Init();
-  // MX_USART1_UART_Init();
-
-  /* Initialize interrupts */
-  // MX_NVIC_Init();
-  /* USER CODE BEGIN 2 */
-
-  /* USER CODE END 2 */
-
-  /* Infinite loop */
-  /* USER CODE BEGIN WHILE */
-  while (1)
-  {
-    rt_thread_mdelay(100);
-    /* USER CODE END WHILE */
-
-    /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
-}
 
 /**
   * @brief System Clock Configuration
